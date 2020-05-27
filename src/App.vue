b5c5df2b675aff8ce45bbf9cabdcc73261b52646
anastasiyaNasevichLeverx <anastasiya.nasevich@leverx.com>
2020-05-27 16:54:08 +0300

    feat: add functionality

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <app-header :maxQuotes="maxQuotes" :quoteCount="quoteCount"></app-header>
+    <app-quote-grid :quotes="quotes" @deleteQuote="deleteQuote"></app-quote-grid>
+    <app-new-quote @newQuote="addNewQuote"></app-new-quote>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from "./components/QuoteGrid";
+  import NewQuote from "./components/NewQuote";
+  import Header from "./components/Header";
+
+  export default {
+    data: function () {
+      return {
+        quotes: [
+          'Just a quote to see something'
+        ],
+        maxQuotes: 10,
+        quoteCount: 1,
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
+    },
+    methods: {
+      addNewQuote(quote) {
+        if (this.quotes.length >= this.maxQuotes) {
+          return alert('Please delete Quotes first!');
+        }
+        this.quotes.push(quote)
+        this.quoteCount += 1
+
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1)
+        this.quoteCount -= 1
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Quotes Added</h3>
+      <div class="progress">
+        <div class="progress">
+          <div class="progress-bar" role="progress-bar" aria-valuenow="60" aria-voluemin="0" aria-voluemax="100"
+          :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
+            {{quoteCount}} / {{maxQuotes}}
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    props: ['quoteCount', 'maxQuotes'],
+    updated() {
+      console.log( this.quoteCount, this.maxQuotes)
+    }
+
+  }
+
+
+</script>
+
+<style>
+
+</style>
